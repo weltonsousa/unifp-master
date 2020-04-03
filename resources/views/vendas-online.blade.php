9e8a2de1a60f8579c5cf051ac0eb2b06eee08f38
Will Rodrigues <wrodrigues153@gmail.com>
2020-04-03 17:14:44 -0300

    ajuste view matricula

--- resources/views/vendas-online.blade.php
+++ resources/views/vendas-online.blade.php
@@ -24,7 +24,7 @@
     <div class="clearfix"></div>
 
     <div class="row">
-       <!-- <div class="col-md-12 col-sm-10 col-xs-12">
+       <div class="col-md-6 col-sm-10 col-xs-6">
             <div class="x_panel">
                 <div class="x_title">
                     <h2>Filtros<small>faça uma busca para encontrar resultados.</small></h2>
@@ -59,7 +59,18 @@
                     </form>
                 </div>
             </div>
-        </div> -->
+        </div>
+        <div class="col-md-6 col-sm-10 col-xs-6">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Resultados</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    
+                </div>
+            </div>
+        </div>
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_content">
