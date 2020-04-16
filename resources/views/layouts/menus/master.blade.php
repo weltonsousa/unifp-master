628f5a1c45c2c1c904914ac9db4f624844c19f7f
Will Rodrigues <wrodrigues153@gmail.com>
2020-04-16 13:56:26 -0300

    ajuste menus

--- resources/views/layouts/menus/master.blade.php
+++ resources/views/layouts/menus/master.blade.php
@@ -23,10 +23,8 @@
                                     <li>
                                         <a><i class="fa fa-laptop"></i> Cursos Online <span class="fa fa-chevron-down"></span></a>
                                         <ul class="nav child_menu">
-                                            <li><a href="{{ route('report-vendas') }}"> Matriculas </a> </li>
-                                            <li><a href="{{ route('report-leads') }}"> Leads </a> </li>
+                                        <li><a href="{{ route('report-vendas') }}"> Matriculas </a> </li>
                                             <li><a href="{{ route('report-boletos') }}"> Pagamentos Pendentes </a> </li>
-                                            <li><a href="{{ route('report-leads-externos') }}"> Leads Externos </a> </li>
-                                            <li><a href="{{ route('report-funil-vendas') }}"> Funil de Vendas </a> </li>
+                                            <li><a href="{{ route('matriculas') }}"> Leads </a> </li>
                                         </ul>
                                     </li>
